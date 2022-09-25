6189f65c1ea651252d05e961c9894f074ba1e0a3
obaida <obaidashurbaji99@gmail.com>
2022-09-25 09:10:47 +0200

    finishing with scroll bar

--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -32,12 +32,10 @@ const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
 		{data.map((item) => (
 			<Box
 				key={item.id || item}
-				itemID={item.id || item}
+				itemId={item.id || item}
 				title={item.id || item}
 				m="0 40px"
 			>
-					{/* <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> */}
-
 				{bodyParts ? (
 					<BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
 				) : (
