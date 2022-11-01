2aa43f3e2a6c11a79e96b3ef924a1002d9dae068
obaida <obaidashurbaji99@gmail.com>
2022-11-01 19:41:21 +0200

    get back to finish

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Stack, Typography, Button } from '@mui/material';
 import HeroBannerImage from '../assets/images/banner.png';
+import HeroManBanner from '../assets/images/man.png';
 
 const HeroBanner = () => {
 	return (
@@ -39,7 +40,7 @@ const HeroBanner = () => {
 			>
 				Exercise
 			</Typography>
-			<img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+			<img src={HeroManBanner} alt="banner" className="hero-banner-img" />
 		</Box>
 	);
 };
